0f78e5b178737cbd9ca50983a3cbd3e3bde11ea9
Marco Fernandes <marcofernandes75@gmail.com>
2012-12-05 14:25:15 +0000

    Fixes on iPhone related issues

--- ad.js
+++ ad.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
 	$('#btn-action').click(function(){
 		$('.bubble-actions').toggle();
+		$('#overlay').toggle();
+	});
+
+	$('#overlay').click(function(){
+		$('.bubble-actions').toggle();
+		$('#overlay').toggle();
 	});
 });
+/*
+window.addEventListener("orientationchange", function() {
+  $('#overlay').css('top', '10px')
+  			   .css('bottom', '10px');
+}, false);
+*/
